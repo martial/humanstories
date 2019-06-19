74d41bcd2360887759e38953b225d980f4e699f0
Martial <martialou@gmail.com>
2019-06-19 14:47:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 sudo apt-get update
-sudo apt-get upgrade
 
-curl -sL http://deb.nodesource.com/setup_8.x | sudo bash -
-sudo apt-get install nodejs
+
 curl https://openframeworks.cc/versions/v0.10.1/of_v0.10.1_linuxarmv6l_release.tar.gz --insecure -O
 mkdir openFrameworks
 tar -xvzf of_v0.10.1_linuxarmv6l_release.tar.gz -C openFrameworks
@@ -12,7 +10,6 @@ make Release -C /home/pi/openFrameworks/of_v0.10.1_linuxarmv6l_release/libs/open
 cd /home/pi/openFrameworks/of_v0.10.1_linuxarmv6l_release/addons
 git clone https://github.com/256dpi/ofxMQTT
 git clone https://github.com/bakercp/ofxIpVideoGrabber
-git clone https://github.com/patriciogonzalezvivo/ofxPiTFT
 cd /home/pi/openFrameworks/of_v0.10.1_linuxarmv6l_release/apps
 git clone https://github.com/martial/humanstories
 make

3ab6113c93dade45ca469832f3b7ede4f7eb692e
Martial Geoffre-Rouland <martial@screen-club.com>
2019-06-20 14:38:17 +0300

    Compiling

--- HumanStoriesClient/rpi_startup.sh
+++ HumanStoriesClient/rpi_startup.sh
@@ -4,4 +4,4 @@ echo Maintenance mode > /dev/tty1
 ifconfig wlan0 > /dev/tty1
 echo Got latest version from git, but didn\'t compile > /dev/tty1
 
-# make -j 2 && make RunRelease
+make -j 2 && make RunRelease

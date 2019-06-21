40f552f28a331a8c1d3a2109e02a035a9d0b9f57
= <cristi8@gmail.com>
2019-06-21 17:26:05 +0300

    fixed startup message

--- HumanStoriesClient/rpi_startup.sh
+++ HumanStoriesClient/rpi_startup.sh
@@ -2,6 +2,6 @@
 
 echo Maintenance mode > /dev/tty1
 ifconfig wlan0 > /dev/tty1
-echo Got latest version from git, but didn\'t compile > /dev/tty1
+echo Got latest version from git, compiling... > /dev/tty1
 
 make -j 2 && make RunRelease
